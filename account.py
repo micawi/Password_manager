6411a48e8ec34c2d6e913d95714964f4fd91f9c9
Michael Wierlemann <mica.wierlemann@gmail.com>
2022-05-12 01:45:57 +0200

    Project init

---
+++ account.py
@@ -0,0 +1,9 @@
+class Account():
+    # Properties
+    Name: str;
+    Password: str;
+
+    # Init
+    def __init__(self, name: str, password: str):
+        self.Name = name;
+        self.Password = password;
---
+++ manager.py
@@ -0,0 +1,6 @@
+from account import Account;
+
+
+class PasswordManager():
+    # Properties
+    AccountList: list;
